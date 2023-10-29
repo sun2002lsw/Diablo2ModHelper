8ac66827cba750abc474d239d371e1ac7f2441e1
sun2002lsw <limit_speed@naver.com>
2023-10-29 17:45:25 +0900

    fix install overwrite

--- ModInstaller/util/file.py
+++ ModInstaller/util/file.py
@@ -4,15 +4,17 @@ import json
 import shutil
 
 
+# 모드 설치하기
 def InstallMod(configFileName, resultDir):
-    destPath = getInstallPath(configFileName)
+    dstPath = getInstallPath(configFileName)
     srcPath = os.path.join(os.getcwd(), resultDir)
     if not os.path.exists(srcPath):
         sys.exit("mod result not exist")
 
-    shutil.move(srcPath, destPath)
+    moveAndOverwrite(srcPath, dstPath)
 
 
+# 설치할 폴더 위치를 확인
 def getInstallPath(configFileName):
     absPath = os.path.join(os.getcwd(), configFileName)
     if not os.path.exists(absPath):
@@ -29,3 +31,23 @@ def getInstallPath(configFileName):
     modName = jsonData["modName"]
 
     return os.path.join(basePath, "mods", modName, modName + ".mpq")
+
+
+# https://stackoverflow.com/questions/7419665/python-move-and-overwrite-files-and-folders
+def moveAndOverwrite(srcPath, dstPath):
+    firDirName = os.path.basename(srcPath)
+    dstPath = os.path.join(dstPath, firDirName)
+
+    for src_dir, dirs, files in os.walk(srcPath):
+        dst_dir = src_dir.replace(srcPath, dstPath)
+        if not os.path.exists(dst_dir):
+            os.makedirs(dst_dir)
+        for file_ in files:
+            src_file = os.path.join(src_dir, file_)
+            dst_file = os.path.join(dst_dir, file_)
+            if os.path.exists(dst_file):
+                # in case of the src and dst are the same file
+                if os.path.samefile(src_file, dst_file):
+                    continue
+                os.remove(dst_file)
+            shutil.move(src_file, dst_dir)
